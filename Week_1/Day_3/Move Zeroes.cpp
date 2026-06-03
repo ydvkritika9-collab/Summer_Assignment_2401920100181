81316ff7c9994c8773d799fbe34d495f47cb9d74
ydvkritika9-collab <ydvkritika9@gmail.com>
2026-06-03 19:48:40 +0530

    Create Move Zeroes.cpp

---
+++ Week_1/Day_3/Move Zeroes.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i=0;
+        int n=nums.size();
+        for(int j=0;j<n;j++){
+            if(nums[j]!=0){
+                swap(nums[i],nums[j]);
+                i++;
+            }
+        }
+        
+    }
+};
